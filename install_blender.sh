e3b7b345fc89b6829e4a58d85673feeac2369895
EC2 Default User <ec2-user@ip-172-31-2-20.us-east-2.compute.internal>
2024-03-06 05:54:34 +0000

    startup script

---
+++ install_blender.sh
@@ -0,0 +1,17 @@
+echo "installing the latest blender"
+
+export BLENDER_VERSION="blender-4.0.2-linux-x64"
+export BLENDER_RELEASE="4.0"
+export BLENDER_URL="https://mirror.clarkson.edu/blender/release/Blender$BLENDER_RELEASE/$BLENDER_VERSION.tar.xz"
+
+
+if [ -d /home/ec2-user/$BLENDER_VERSION ]; then
+	echo "Blender already updated. No need to download again"
+else
+	wget "$BLENDER_URL" -P /home/ec2-user/
+	tar -xf /home/ec2-user/$BLENDER_VERSION.tar.xz -C /home/ec2-user/
+	ln -s /home/ec2-user/$BLENDER_VERSION /home/ec2-user/blenderapp
+fi
+
+
+
